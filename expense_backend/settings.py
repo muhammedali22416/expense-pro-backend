95d63b0e9aeb035fb58a355fbe57f047f381b0ce
muhammedali22416 <muhammedali22416@gmail.com>
2026-01-30 18:39:09 +0500

    Final static files fix

--- expense_backend/settings.py
+++ expense_backend/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'corsheaders.middleware.CorsMiddleware', # Sabse upar
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
