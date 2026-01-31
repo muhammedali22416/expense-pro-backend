64f5b3cdbda368f5bc33e2fd487d59c95d08842e
muhammedali22416 <muhammedali22416@gmail.com>
2026-01-31 15:40:40 +0500

    Fixing API routes and static paths

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ExpenseViewSet, IncomeViewSet, login_user, register_user
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 router = DefaultRouter()
 router.register(r'expenses', ExpenseViewSet, basename='expense')
 router.register(r'incomes', IncomeViewSet, basename='income')
 
 urlpatterns = [
-    path('api/login/', login_user),  # Ab ye React wale URL se match karega
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/register/', register_user),
     path('api/', include(router.urls)),
 ]
