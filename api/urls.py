ad1c890482d1987c829038f8dc975920f560f818
muhammedali22416 <muhammedali22416@gmail.com>
2026-01-31 16:29:04 +0500

    vercel.json update1

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ExpenseViewSet, IncomeViewSet, login_user, register_user
+from .views import ExpenseViewSet, IncomeViewSet, register_user
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 router = DefaultRouter()
+# Ye ab /api/expenses/ aur /api/incomes/ banayega
 router.register(r'expenses', ExpenseViewSet, basename='expense')
 router.register(r'incomes', IncomeViewSet, basename='income')
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/register/', register_user),
-    path('api/', include(router.urls)),
+    path('register/', register_user),
+    # Yahan khali string ('') rakhein kyunke main urls.py mein 'api/' pehle se hai
+    path('', include(router.urls)), 
 ]
