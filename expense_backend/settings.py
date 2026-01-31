1173e24ceef50c61de6fd425c9158d5b6a308d7f
muhammedali22416 <muhammedali22416@gmail.com>
2026-01-31 15:29:25 +0500

    Include static files in deployment

--- expense_backend/settings.py
+++ expense_backend/settings.py
@@ -58,7 +58,6 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
